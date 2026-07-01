d1cc559ae574fca320da0fd42fee2b5830a4e673
shivam verma <8081verma@gmail.com>
2026-07-01 15:33:06 +0530

    Time: 11 ms (20.72%), Space: 21.8 MB (5.05%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+
+    void topoBFS(int n , vector<int> &ans , unordered_map<int , vector<int>> &adjList){
+
+        queue<int> q;
+        unordered_map<int , int> indegree;
+
+        //indegree map ready kro
+        for(int src =0; src<n; src++){
+            for(auto nbr : adjList[src]){
+                indegree[nbr]++;
+            }
+        }
+
+        //check for indegree 0 and push in q
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        //main logic
+        while(!q.empty()){
+            int front = q.front();
+            ans.push_back(front);
+            q.pop();
+
+            for(auto nbr : adjList[front]){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0){
+                    q.push(nbr);
+                }
+            }
+        }
+
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+
+        unordered_map<int , vector<int>> adjList;
+        for(auto i : prerequisites){
+            int u = i[1];
+            int v = i[0];
+
+            adjList[u].push_back(v);
+        }
+        // unordered_map<int , bool> visited;
+        vector<int> ans; 
+
+        topoBFS(n ,  ans , adjList);
+
+        if(ans.size() != n) //cycle present h 
+            return false;
+
+        else return true;
+    }
+};
