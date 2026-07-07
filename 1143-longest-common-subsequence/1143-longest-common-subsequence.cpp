d40c8f072e5687c12b0b54d600a1353a381f9bfe
shivam verma <8081verma@gmail.com>
2026-07-07 16:06:27 +0530

    Time: 26 ms (65.05%), Space: 27.3 MB (68.88%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -39,16 +39,51 @@ public:
         return dp[i][j];
     }
 
+    //Bottom up Approach
+    int solveUsingTab(string &text1 , string &text2 , int i , int j){
+        int n1 = text1.length();
+        int n2 = text2.length();
+
+        //step1 -> create 2D dp
+        vector<vector<int>> dp(n1+1 , vector<int>(n2+1 , 0));
+
+        //step 2-> Analyze base case and update DP
+        //Already done in step 1
+
+        //step 3-> reverse kro aur loop chalao
+        //i ->[0 to n1]
+        //j ->[0 to n2]
+        for(int i = n1-1; i>=0; i--){
+            for(int j = n2-1; j>=0; j--){
+                int ans = 0;
+                if(text1[i] == text2[j]){
+                    //match
+                    ans = 1 + dp[i+1][j+1];
+                }
+                else{
+                    //no match
+                    ans = 0 + max(dp[i+1][j] , dp[i][j+1]);
+                }
+                dp[i][j] = ans;                
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         int i = 0;
         int j = 0;
 
         // return solveUsingRE(text1 , text2 , i , j);
-        int n1 = text1.length();
-        int n2 = text2.length();
+        // int n1 = text1.length();
+        // int n2 = text2.length();
 
         //step1->create 2d Dp
-        vector<vector<int>> dp(n1+1 , vector<int>(n2+1 , -1));
-        return solveUsingMemo(text1 , text2 , i , j , dp);
+        // vector<vector<int>> dp(n1+1 , vector<int>(n2+1 , -1));
+        // return solveUsingMemo(text1 , text2 , i , j , dp);
+
+        return solveUsingTab(text1 , text2 , i , j);
+
     }
 };
