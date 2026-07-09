a0bdabc60faeb350e78c091dc119284c48e4cbc8
shivam verma <8081verma@gmail.com>
2026-07-09 17:33:54 +0530

    Time: 34 ms (85.99%), Space: 11.1 MB (54.91%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -27,13 +27,42 @@ public:
         return dp[s][e];
     }
 
+    int solveUsingTab(int n){
+
+        //step1-> create dp
+        vector<vector<int>> dp(n+2 , vector<int>(n+2 , 0));
+
+        //step 2-> analyze base case
+
+        //step3->reverse kro loop chalao
+        //s->[1 to n]
+        //e->[n to s]
+        for(int s=n; s>=1; s--){
+            for(int e=s; e<=n; e++){
+                if(s >= e) continue;
+                else{
+                    int ans = INT_MAX;
+                    for(int i=s; i<e; i++){
+                        ans = min(ans , i + max(dp[s][i-1] , dp[i+1][e]));
+                    }
+                    dp[s][e] = ans;
+                }
+            }
+        }
+        return dp[1][n];
+
+    }
+
+
     int getMoneyAmount(int n) {
 
         // return solveUsingRE(1 , n);
 
         //step1-> create 2d DP array
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
-        return solveUsingMem(1 , n , dp);
+        // vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+        // return solveUsingMem(1 , n , dp);
+
+        return solveUsingTab(n);
 
     }
 };
