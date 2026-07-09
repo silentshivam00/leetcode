9edbe0fc32a2739dd37c9c3c34a3516bbbcd687b
shivam verma <8081verma@gmail.com>
2026-07-09 11:24:47 +0530

    Time: 13 ms (32.97%), Space: 15.2 MB (33.85%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -26,6 +26,7 @@ public:
     }
     //2D DP Apply
 
+    //Top down Approach
     int solveUsingMem(vector<vector<int>>& cuboids , int curr , int prev , vector<vector<int>> &dp){
         //base case
         if(curr >= cuboids.size()){
@@ -42,6 +43,31 @@ public:
         int exc = 0 + solveUsingMem(cuboids , curr+1 , prev , dp);
         dp[curr][prev+1] = max(inc , exc);
         return dp[curr][prev+1];
+    } 
+
+    //Bottom up approach
+    int solveUsingTab(vector<vector<int>>& cuboids , int curr , int prev){
+        int n = cuboids.size();
+        //step 1-> create 2d DP
+        vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
+        //step 2-> analyze base case
+        //already done in step 1
+
+        //step 3-> reverkro aur loop chalao
+        //curr ->[0 ,n]
+        //prev ->[-1 , curr]
+        for(int curr = n-1; curr>=0; curr--){
+            for(int prev = curr-1; prev>=-1; prev--){
+                int inc = 0;
+                if(prev == -1 || isSafePlacing(cuboids[prev] , cuboids[curr])){
+                    inc = cuboids[curr][2] + dp[curr+1][curr+1];
+                }
+                int exc = 0 + dp[curr+1][prev+1];
+                dp[curr][prev+1] = max(inc , exc);
+            }
+        }
+        return dp[0][0];
+       
     }    
 
     int maxHeight(vector<vector<int>>& cuboids) {
@@ -57,7 +83,10 @@ public:
         // return solveUsingRE(cuboids , curr , prev);
 
         //step 1-> create 2d dp array
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
-        return solveUsingMem(cuboids , curr , prev , dp);
+        // vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+        // return solveUsingMem(cuboids , curr , prev , dp);
+
+        return solveUsingTab(cuboids , curr , prev);
+
     }
 };
