6f77e00966c2b0bf5683327f75956641b9fd5681
shivam verma <8081verma@gmail.com>
2026-07-09 11:53:22 +0530

    Time: 5 ms (83.02%), Space: 13.2 MB (67.25%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -46,7 +46,7 @@ public:
     } 
 
     //Bottom up approach
-    int solveUsingTab(vector<vector<int>>& cuboids , int curr , int prev){
+    int solveUsingTab(vector<vector<int>>& cuboids){
         int n = cuboids.size();
         //step 1-> create 2d DP
         vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
@@ -54,8 +54,8 @@ public:
         //already done in step 1
 
         //step 3-> reverkro aur loop chalao
-        //curr ->[0 ,n]
-        //prev ->[-1 , curr]
+        //curr ->[0 to n]
+        //prev ->[-1 to curr]
         for(int curr = n-1; curr>=0; curr--){
             for(int prev = curr-1; prev>=-1; prev--){
                 int inc = 0;
@@ -67,8 +67,51 @@ public:
             }
         }
         return dp[0][0];
-       
-    }    
+    } 
+
+    int solveUsingTabSO(vector<vector<int>>& cuboids){
+        int n = cuboids.size();
+        vector<int>prevRow(n+1 , 0);
+        vector<int>currRow(n+1 , 0);
+        //step 3-> reverkro aur loop chalao
+        //curr ->[0 to n]
+        //prev ->[-1 to curr]
+        for(int curr = n-1; curr>=0; curr--){
+            for(int prev = curr-1; prev>=-1; prev--){
+                int inc = 0;
+                if(prev == -1 || isSafePlacing(cuboids[prev] , cuboids[curr])){
+                    inc = cuboids[curr][2] + prevRow[curr+1];
+                }
+                int exc = 0 + prevRow[prev+1];
+                currRow[prev+1] = max(inc , exc);
+            }
+            //shifting
+            prevRow = currRow;
+        }
+        return prevRow[0];
+    }           
+
+    int solveUsingTabSO2(vector<vector<int>>& cuboids){
+        int n = cuboids.size();
+        // vector<int>prevRow(n+1 , 0);
+        vector<int>currRow(n+1 , 0);
+        //step 3-> reverkro aur loop chalao
+        //curr ->[0 to n]
+        //prev ->[-1 to curr]
+        for(int curr = n-1; curr>=0; curr--){
+            for(int prev = -1; prev<=curr-1; prev++){
+                int inc = 0;
+                if(prev == -1 || isSafePlacing(cuboids[prev] , cuboids[curr])){
+                    inc = cuboids[curr][2] + currRow[curr+1];
+                }
+                int exc = 0 + currRow[prev+1];
+                currRow[prev+1] = max(inc , exc);
+            }
+            //shifting
+            // prevRow = currRow;
+        }
+        return currRow[0];
+    }  
 
     int maxHeight(vector<vector<int>>& cuboids) {
         
@@ -86,7 +129,11 @@ public:
         // vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
         // return solveUsingMem(cuboids , curr , prev , dp);
 
-        return solveUsingTab(cuboids , curr , prev);
+        // return solveUsingTab(cuboids , curr , prev);
+
+        // return solveUsingTabSO(cuboids);
+
+        return solveUsingTabSO2(cuboids);
 
     }
 };
