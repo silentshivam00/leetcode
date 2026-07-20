5687bb6aa9582e299871d47cdb6277f67d3deece
shivam verma <8081verma@gmail.com>
2026-07-20 12:23:42 +0530

    Time: 3 ms (18.83%), Space: 8.9 MB (80.99%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        string ans;
+        int c = 0;
+
+        while(i>=0 || j>=0 || c){
+            int sum = c;
+            if(i >= 0){
+                sum += a[i] - '0';
+                i--;
+            } 
+            if(j >= 0){
+                sum += b[j] - '0';
+                j--;
+            }
+            ans.push_back((sum % 2) + '0');
+            c = sum/2;
+        }
+
+        reverse(ans.begin() , ans.end());
+        return ans;
+    }
+};
