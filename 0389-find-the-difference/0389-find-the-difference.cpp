c1fc1ea133900d73751b7bf3efcd54103b8328bd
shivam verma <8081verma@gmail.com>
2026-07-21 17:48:44 +0530

    Time: 0 ms (100%), Space: 9.3 MB (51.9%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+
+        for(char ch : s)
+            ans ^= ch;
+
+        for(char ch : t)
+            ans ^= ch;
+
+        return ans;
+    }
+};
